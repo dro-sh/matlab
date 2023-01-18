69fac60eac3b7076e24b4d810f25687dc1e76385
Мирон Леонов <37750417+MironLeonov@users.noreply.github.com>
2023-01-18 20:19:24 +0300

    Update script.m

--- 3-task/3/script.m
+++ 3-task/3/script.m
@@ -1,14 +1,13 @@
 number_of_runs = 10000;
-hits_first_sister = zeros(1,number_of_runs); 
-hits = zeros(1,number_of_runs); 
 
-for n = 1:number_of_runs
-    was_boy = 1;
-    was_girl = 0;
-    second_kid = randi([0, 1], 1);
-    if was_boy ~= second_kid
-        hits(n) = 1;
-    end
-end
+families = randi(2,number_of_runs,2);
+families_without_brothers = ~all(families==1,2);
+families_with_diff_kids = any(families==1,2)&any(families==2,2);
+families_with_sisters = all(families==2,2);
 
-disp(min(1-binocdf(sum(hits),number_of_runs,0.5),binocdf(sum(hits),number_of_runs,0.5))/2)
+
+[~,pci] = binofit(sum(families_with_diff_kids), sum(families_without_brothers),0.05);
+disp(pci)
+
+[~,pci] = binofit(sum(families_with_sisters), sum(families_without_brothers),0.05);
+disp(pci)
